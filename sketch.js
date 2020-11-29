c490c44ba4f1c27d1e772ca7f076f94dbc3786d0
Queeneedu <73861561+Queeneedu@users.noreply.github.com>
2020-11-29 23:46:59 +0900

    캔버스사이즈 조정, 슬라이더 가져오기

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let informant;
+let importance
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1920, 1080);
+  background(0);
+  informant = new Informant();
 }
 
 function draw() {
-  background(220);
+
+
+
+
+let slider;
+function setup() {
+  slider = createSlider(0, 255, 100);
+  slider.position(10, 10);
+  slider.style('width', '80px');
+}
+
+function draw() {
+  let val = slider.value();
+  background(val);
 }
