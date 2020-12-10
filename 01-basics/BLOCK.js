02a627d4c9f208d380aeeb4e5211ea4417e359e8
Queeneedu <73861561+Queeneedu@users.noreply.github.com>
2020-12-11 02:15:57 +0900

    땅과 벽 만들기

--- 01-basics/BLOCK.js
+++ 01-basics/BLOCK.js
@@ -1,65 +1,20 @@
-// Benedikt Groß
-// Example is based on examples from: http://brm.io/matter-js/, https://github.com/shiffman/p5-matter
-let informant;
-let importance;
-let myFont;
+function Block(x, y, w, h) {
+  this.body = Bodies.rectangle(x, y, w, h);
+  this.w = w;
+  this.h = h;
+  World.add(world, this.body);
 
-
-var Engine = Matter.Engine;
-var Render = Matter.Render;
-var World = Matter.World;
-var Bodies = Matter.Bodies;
-
-var engine;
-
-var boxA;
-var boxB;
-var boxC;
-var ground;
-
-
-
-function setup() {
-
-  createCanvas(1920, 1080);
-  background(255);
-
-//  create an engine
-  engine = Engine.create();
-
-  // create two boxes and a ground
-  boxA = Bodies.rectangle(500,0, 100, 200);
-  boxB = Bodies.circle(270, 50, 50);
-  boxC = Bodies.rectangle(500,50,500,10);
-  ground = Bodies.rectangle(0, height, width, 20,
-     {isStatic: true}
-   );
-
-  // add all of the bodies to the world
-  World.add(engine.world, [boxA, boxB,boxC, ground]);
-
-  // run the engine
-  Engine.run(engine);
-}
-
-function draw() {
-  background(0);
-
-  fill(255);
-  drawVertices(boxA.vertices);
-  drawVertices(boxB.vertices);
-  // drawVertices(boxC.vertices);
-
-  fill(128);
-  drawVertices(ground.vertices);
-
-
-}
-
-function drawVertices(vertices) {
-  beginShape();
-  for (var i = 0; i < vertices.length; i++) {
-    vertex(vertices[i].x, vertices[i].y);
-  }
-  endShape(CLOSE);
+  this.show = function() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(1);
+    stroke(255);
+    fill(127);
+    rect(0, 0, this.w, this.h);
+    pop();
+  };
 }
--- 01-basics/sketch.js
+++ 01-basics/sketch.js
@@ -12,8 +12,9 @@ let Render = Matter.Render;
 let World = Matter.World;
 let Bodies = Matter.Bodies;
 let engine;
-let boxA, ground;
-
+let world;
+let groundA,groundB,groundC;
+let blocks = [];
 
 //font load
 function preload() {
@@ -23,6 +24,7 @@ function preload() {
 
 
 function setup(){
+
   //informant
   state = 1;
   createCanvas(1920, 1080);
@@ -32,54 +34,76 @@ function setup(){
   typing = createInput('Enter Information');
   typing.position(width/2-typing.width, height/2-200);
   typing.size(500,100);
-  //  typing.style('font','basicFont');
   typing.style('font-size','60px');
 
   //Sendbutton
   button = createButton('Send');
-  //  button.style('font','basicFont');
   button.style('font-size','60px');
   button.position(width/2, height/2 + 200);
   button.size(200,100);
   button.mousePressed(upload);
 
   //slider
-  slider = createSlider(1,100,1); //(min,max,start from 0)
-  slider.position(width/2,height/2);
-  slider.style('width','200px');
-  slider.style('height','100px');
+  slider = createSlider(1,200,100); //(min,max,start from 0)
+  slider.position(width/2-100, height/2+30);
+  slider.style('width','400px');
+  slider.style('height','50px');
 
   //Let's make the World
   engine = Engine.create();
-  boxA = Bodies.rectangle(500,0, 100, 200);
-  ground = Bodies.rectangle(0, height, width, 20,
-       {isStatic: true}
-     );
-  World.add(engine.world, [boxA, ground]); //박스는 나중에 추가하기
+  world = engine.world;
   Engine.run(engine);
+
+  //ground and wall
+  var options = {
+    isStatic: true
+  };
+  groundA = Bodies.rectangle(width/2, height, width, 10, options);
+  groundB = Bodies.rectangle(0, height/2, 10, height, options);
+  groundC = Bodies.rectangle(width, height/2, 10, height, options);
+
+  World.add(world, groundA);
+  World.add(world, groundB);
+  World.add(world, groundC);
 }
 
 //button and upload
 function upload(){
   info = typing.value();
-  infoSize = slider.value();
-  state = 3;
+  infoSize = map(slider.value(),1,100,100,500);
+  blocks.push(new Block(width/2,0,infoSize,infoSize/2));
+  state = 2;
 }
 
+// function MakeBox(){
+//   boxA = Bodies.rectangle(500,0, 100, 200);
+
+//   World.add(engine.world, [boxA, ground]); //박스는 나중에 추가하기
+//   Engine.run(engine);
+// }
 
 function draw(){
-
   //informant
   if (state == 1){
   }
 
-  //upload text and fall
+
   else if (state == 2){
+    //upload text and fall
+    background(255,200,0);
+    Engine.update(engine);
+    for (var i = 0; i < blocks.length; i++) {
+    blocks[i].show();
+    }
+
+    //show ground
+    noStroke(255);
     fill(255);
-    drawVertices(boxA.vertices);
-    fill(128);
-    drawVertices(ground.vertices);
-  }
+    rectMode(CENTER);
+    rect(groundA.position.x, groundA.position.y, width, 20);
+    rect(groundB.position.x, groundB.position.y, 10, height);
+    rect(groundC.position.x, groundC.position.y, 10, height);
+    }
 
   //INEEDREST
   else if (state == 3){
@@ -89,12 +113,4 @@ function draw(){
     textAlign(CENTER,CENTER);
     text('INEEDAREST',width/2, height/2);
   }
-}
-
-function drawVertices(vertices) {
-  beginShape();
-  for (var i = 0; i < vertices.length; i++) {
-    vertex(vertices[i].x, vertices[i].y);
-  }
-  endShape(CLOSE);
 }
