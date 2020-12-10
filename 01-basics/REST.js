4394fa6285c4c44c10ddf4f90aa9f7148771131e
Queeneedu <73861561+Queeneedu@users.noreply.github.com>
2020-12-11 02:15:42 +0900

    matter test

--- 01-basics/REST.js
+++ 01-basics/REST.js
@@ -1,21 +1,46 @@
+var Engine = Matter.Engine,
+  // Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
 
-let font;
-let textString = 'MOVE';
-function preload() {
-  font = loadFont('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/SangSangFlowerRoad.woff');
-}
+var engine;
+var world;
+var blocks = [];
+var block;
+var groundA, groundB, groundC;
+
 function setup() {
-  createCanvas(1080,720);
-  background(210);
-  textAlign(CENTER);
-  let bbox = font.textBounds(textString, width/2, height/2,300,CENTER,CENTER);
-  fill(255);
-  stroke(0);
-  rect(bbox.x, bbox.y, bbox.w, bbox.h);
-  fill(0);
-  noStroke();
+  createCanvas(500, 500);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  var options = {
+    isStatic: true
+  };
+  groundA = Bodies.rectangle(250, height, width, 20, options);
+  groundB = Bodies.rectangle(0, 250, 10, height, options);
+  groundC = Bodies.rectangle(height, 250, 10, height, options);
 
-  textFont(font);
-  textSize(300);
-  text(textString, width/2, height/2);
+  World.add(world, groundA);
+  World.add(world, groundB);
+  World.add(world, groundC);
+}
+
+
+function mouseDragged() {
+  blocks.push(new Block(mouseX, mouseY, random(10, 40), random(10, 40)));
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+  for (var i = 0; i < blocks.length; i++) {
+    blocks[i].show();
+  }
+  noStroke(255);
+  fill(170);
+  rectMode(CENTER);
+  rect(groundA.position.x, groundA.position.y, width, 20);
+  rect(groundB.position.x, groundB.position.y, 10, height);
+  rect(groundC.position.x, groundC.position.y, 10, height);
 }
